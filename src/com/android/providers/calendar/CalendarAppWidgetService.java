2023e49266668292188171888e3d99f19c738ad4
Chen Mike <mike.chen@sonyericsson.com>
2010-11-12 08:17:48 +0100

    Dont use the default locale when formatting SQL statements
    It is not safe to use the default locale when using String.format
    to produce SQL statements. Some locales will break the SQL
    and as a consequence crash the app.
    
    Change-Id: I302e0f65108d6ef75610361626779fdb8143d4e6

--- src/com/android/providers/calendar/CalendarAppWidgetService.java
+++ src/com/android/providers/calendar/CalendarAppWidgetService.java
@@ -39,6 +39,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.RemoteViews;
 
+import java.util.Locale;
 import java.util.Set;
 import java.util.TimeZone;
 
@@ -566,9 +567,9 @@ public class CalendarAppWidgetService extends Service implements Runnable {
         long end = now + searchDuration;
 
         Uri uri = Uri.withAppendedPath(Instances.CONTENT_URI,
-                String.format("%d/%d", now, end));
+                String.format((Locale)null, "%d/%d", now, end));
 
-        String selection = String.format("%s=1 AND %s!=%d",
+        String selection = String.format((Locale)null, "%s=1 AND %s!=%d",
                 Calendars.SELECTED, Instances.SELF_ATTENDEE_STATUS,
                 Attendees.ATTENDEE_STATUS_DECLINED);
 
